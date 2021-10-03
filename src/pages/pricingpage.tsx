603a178d48cd19ccb226fc475b24fd6a59f1cde8
John McCarthy <jemccarthy13@gmail.com>
2021-10-03 16:26:00 +0200

    pricing page

--- src/pages/pricingpage.tsx
+++ src/pages/pricingpage.tsx
@@ -1,14 +1,68 @@
+import { Link } from "@mui/material";
 import React from "react";
 
+import { PricingTable, PricingSlot, PricingDetail } from "react-pricing-table";
+
 export default class PricingPage extends React.PureComponent {
+  slotClick = () => {
+    console.log("book?");
+  };
+
   render() {
     return (
       <div className="page-content">
         <div className="page-header">Pricing</div>
-        <div>30 minute</div>
-        <div>45 minute</div>
-        <div>Seasonal</div>
-        <div>Special Events</div>
+        <PricingTable highlightColor="#87059c">
+          <PricingSlot
+            onClick={this.slotClick}
+            title="30-minute"
+            priceText="$75"
+            buttonText="Book Now"
+          >
+            <PricingDetail>
+              <b>20+</b> digital images
+            </PricingDetail>
+            <PricingDetail>
+              <b>Full</b> print release
+            </PricingDetail>
+          </PricingSlot>
+          <PricingSlot
+            title="45-minute"
+            priceText="$95"
+            buttonText="Book Now"
+            onClick={this.slotClick}
+          >
+            <PricingDetail>
+              <b>35+</b> digital images
+            </PricingDetail>
+            <PricingDetail>
+              <b>Full</b> print release
+            </PricingDetail>
+          </PricingSlot>
+          <PricingSlot
+            title="Seasonal"
+            priceText="TBD"
+            buttonText="Book Now"
+            onClick={this.slotClick}
+          >
+            <PricingDetail>
+              Details will be published on
+              <Link href="https://www.facebook.com/clairemariefotografie">
+                Facebook
+              </Link>
+            </PricingDetail>
+          </PricingSlot>
+          <PricingSlot
+            title="Packages"
+            priceText="Contact Me"
+            buttonText="Book Now"
+            onClick={this.slotClick}
+          >
+            <PricingDetail>Birthdays</PricingDetail>
+            <PricingDetail>Weddings</PricingDetail>
+            <PricingDetail>Special Events</PricingDetail>
+          </PricingSlot>
+        </PricingTable>
       </div>
     );
   }
