56f3cc4898db7155439c8bc842a0382df148fd6e
John McCarthy <jemccarthy13@gmail.com>
2021-11-02 21:52:40 +0100

    cleanup

--- public/api/pricing/pricing.php
+++ public/api/pricing/pricing.php
@@ -1,5 +1,4 @@
 <?php
-//require "../bootstrap.php";
 require "./pricingcontroller.php";
 require "../dbconnect.php";
 
@@ -8,21 +7,10 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
-
-// all of our endpoints start with /person
-// everything else results in a 404 Not Found
-if ($uri[2] !== 'pricing') {
-    header("HTTP/1.1 404 Not Found");
-    echo json_encode($uri);
-    exit();
-}
-
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-
 
 // pass the request method and user ID to the PersonController and process the HTTP request:
 $dbConnection = new Database();
 $controller = new PricingController($dbConnection, $requestMethod);
 $controller->processRequest();
+?>
