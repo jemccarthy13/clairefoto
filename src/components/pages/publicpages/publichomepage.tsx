a0b79cd7cb58bb4fd379e3e1e9abebaca222fb12
John McCarthy <jemccarthy13@gmail.com>
2021-11-05 17:15:48 +0100

    convert to functional / comment

--- src/components/pages/publicpages/publichomepage.tsx
+++ src/components/pages/publicpages/publichomepage.tsx
@@ -1,34 +1,35 @@
-import React from "react";
-
-export default class PublicHomePage extends React.PureComponent {
-  render() {
-    return (
-      <div className="page-content">
-        <div className="page-header">About Me</div>
-        <div style={{ marginLeft: "50px", marginRight: "50px" }}>
-          <img
-            style={{
-              width: "100%",
-              textAlign: "center",
-            }}
-            alt="aboutme_camera"
-            src="/images/dashboard/aboutme_camera.png"
-          />
-        </div>
-        <div
+/**
+ * A functional component to render the (static) public home page.
+ *
+ * @returns JSX.Element of the public home page
+ */
+export default function PublicHomePage(): JSX.Element {
+  return (
+    <div className="page-content">
+      <div className="page-header">About Me</div>
+      <div style={{ marginLeft: "50px", marginRight: "50px" }}>
+        <img
           style={{
+            width: "100%",
             textAlign: "center",
-            paddingTop: "20px",
-            marginLeft: "50px",
-            marginRight: "50px",
           }}
-        >
-          Hi and welcome to my page. My name is Claire and I love capturing
-          precious moments for others. Feel free to contact me in either
-          English, German or French. Please <a href="/#/contact">reach out</a>{" "}
-          if you have any questions.
-        </div>
+          alt="aboutme_camera"
+          src="/images/dashboard/aboutme_camera.png"
+        />
       </div>
-    );
-  }
+      <div
+        style={{
+          textAlign: "center",
+          paddingTop: "20px",
+          marginLeft: "50px",
+          marginRight: "50px",
+        }}
+      >
+        Hi and welcome to my page. My name is Claire and I love capturing
+        precious moments for others. Feel free to contact me in either English,
+        German or French. Please <a href="/#/contact">reach out</a> if you have
+        any questions.
+      </div>
+    </div>
+  );
 }
