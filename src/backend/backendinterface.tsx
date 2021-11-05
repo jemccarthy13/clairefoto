5da335d32ab3e4a91c09df473007aec952b3588b
John McCarthy <jemccarthy13@gmail.com>
2021-11-05 18:46:23 +0100

    commenting

--- src/backend/backendinterface.tsx
+++ src/backend/backendinterface.tsx
@@ -1,3 +1,12 @@
+/**
+ * Contains information on an image.
+ *
+ * @member {string} title Title of the image
+ * @member {number} width Width of the image
+ * @member {number} height Height of the image
+ * @member {string} srcSet Source set for the image
+ * @member {string} src Source path of the image
+ */
 export interface ImageList {
   title: string;
   width: number;
@@ -6,7 +15,15 @@ export interface ImageList {
   src: string;
 }
 
-export type PricingData = {
+/**
+ * Contains information on a pricing option.
+ *
+ * @member {string} id ID number of the price option
+ * @member {string} title Title of the price option
+ * @member {string} price Price cost of the price option
+ * @member {Object} options num_images, print_rel, custom_text
+ */
+export interface PricingData {
   id: string;
   title: string;
   price: string;
@@ -16,7 +33,7 @@ export type PricingData = {
     custom_txt: string[];
   };
   booking: boolean;
-};
+}
 
 export type GoogleDateTime = {
   dateTime: string;
--- src/components/pages/publicpages/portfolio/photopage.tsx
+++ src/components/pages/publicpages/portfolio/photopage.tsx
@@ -1,37 +1,68 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
+// Internal Utilities
 import backend from "../../../../backend/backend";
-
 import { ImageList } from "../../../../backend/backendinterface";
 import SnackActions from "../../../../alert/alert";
-
-import ModalCarouselAdapter from "./modalcarouseladapter";
-import GalleryAdapter from "./galleryadapter";
 import { useConfirmDialog } from "../../../useConfirmDialog";
-import DropzoneAdapter from "./dropzoneadapter";
 import { AuthContext } from "../../../authcontext";
 
+// Adapter pattern for external libraries
+import ModalCarouselAdapter from "./modalcarouseladapter";
+import GalleryAdapter from "./galleryadapter";
+import DropzoneAdapter from "./dropzoneadapter";
+
+/**
+ * Interface for the photo page props
+ *
+ * @member {string} title: Title of the Photo Page
+ * @member {string} serverDir: Directory for the photos on the server
+ */
 interface PPProps {
   title: string;
   serverDir: string;
 }
 
-export default function PhotoPage(props: PPProps) {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
+/**
+ * Functional component for a photo page gallery
+ *
+ * @param props Title of the page and serverDir where to get the photos
+ * @returns JSX.Element to render the photo gallery
+ */
+export default function PhotoPage(props: PPProps): JSX.Element {
   const defLst: ImageList[] = [];
+  // List of images from the server
   const [photos, setImgs] = useState(defLst);
 
+  // Index of the clicked on image
+  const [currentImage, setCurrentImage] = useState(0);
+
+  // Flag for whether the modal viewer is open
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  // Flag for the delete dialog
   const [dialogOpen, setDialogOpen] = useState(false);
+
+  // Index of the photo to delete
   const [delIndex, setDelIndex] = useState(-1);
 
+  /**
+   * Custom effect - when the serverDir props changes, fetch
+   * images from the server, then set the current list of
+   * photos to render
+   */
   useEffect(() => {
     backend.getImages(props.serverDir).then(async (data) => {
       setImgs(data);
     });
   }, [props.serverDir]);
 
+  /**
+   * Open the modal viewer at the current index by setting state/flags
+   *
+   * @param index Current index of clicked photo
+   * @returns a function to open the viewer
+   */
   const openViewer = (index: number) => {
     return () => {
       setCurrentImage(index);
@@ -39,15 +70,30 @@ export default function PhotoPage(props: PPProps) {
     };
   };
 
-  const customContext = (event: any) => {
+  /**
+   * Right clicking on an image does nothing, because this
+   * function overrides the default context menu
+   *
+   * @param event a React event for onContextMenu
+   */
+  const customContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
 
+  /**
+   * Close the modal lightbox viewer by setting state / flags
+   */
   const closeLightbox = () => {
     setCurrentImage(0);
     setViewerIsOpen(false);
   };
 
+  /**
+   * Custom effect of using a confirmation dialog to
+   * perform an action; in this case, delete an image
+   *
+   * @param dialogOpen Dependancy to toggle the delete dialog
+   */
   const DelDialog = useConfirmDialog(
     {
       title: "Confirm delete?",
@@ -60,6 +106,7 @@ export default function PhotoPage(props: PPProps) {
           backend.deleteImage(photos[delIndex].src).then((data) => {
             if (data.ok) {
               SnackActions.success("Deleted " + photos[delIndex].src);
+              // remove the image from the currently displayed list
               const newImgs = photos;
               newImgs.splice(delIndex, 1);
               setImgs(
@@ -83,9 +130,15 @@ export default function PhotoPage(props: PPProps) {
     [dialogOpen]
   );
 
+  /**
+   * Delete a photo from the given index when an admin clicks the
+   * delete icon.
+   *
+   * @param index index of the photo to delete
+   * @returns function to be used as onClick
+   */
   const deletePhoto = (index: number) => {
     const func = () => {
-      console.log("set del index" + index);
       setDelIndex(index);
       setDialogOpen(true);
     };
@@ -94,25 +147,32 @@ export default function PhotoPage(props: PPProps) {
   };
 
   return (
+    /** Set the context menu for the page */
     <div
       className="page-content"
       style={{ paddingBottom: "100px" }}
-      onContextMenu={customContext}
+      onContextMenu={customContextMenu}
     >
       <div className="page-header">{props.title}</div>
 
+      {/** If authorized, provide the dropzone to add images to the gallery */}
       <AuthContext.Consumer>
         {(value) =>
           value.auth && <DropzoneAdapter destination={props.serverDir} />
         }
       </AuthContext.Consumer>
 
+      {/**
+       * Using the photos from the server, render the third-party gallery
+       * using the adapter pattern
+       */}
       <GalleryAdapter
         photos={photos}
         openViewer={openViewer}
         onDelete={deletePhoto}
       />
 
+      {/** Modal Carousel for when an image is clicked */}
       <ModalCarouselAdapter
         isOpen={viewerIsOpen}
         photos={photos}
@@ -120,6 +180,7 @@ export default function PhotoPage(props: PPProps) {
         onClose={closeLightbox}
       />
 
+      {/** Delete dialog when the trash icon button is clicked */}
       {DelDialog}
     </div>
   );
