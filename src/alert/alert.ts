daf58fa717fa3e555a753cc93560f3a9c7d64a59
John McCarthy <jemccarthy13@gmail.com>
2021-11-05 19:55:31 +0100

    commenting

--- src/alert/alert.ts
+++ src/alert/alert.ts
@@ -6,28 +6,67 @@ import {
 } from "notistack";
 import React from "react";
 
+// Set up a global snackbar reference for use to enqueue and dismiss
+// snack notifications
 let snackbarRef: WithSnackbarProps;
 export const SnackbarUtilsConfigurator: React.FC = () => {
   snackbarRef = useSnackbar();
   return null;
 };
 
+/**
+ * The SnackActions global exported object to make notifications
+ */
 const SnackActions = {
+  /**
+   * Enqueue a green snackbar notification (success)
+   * @param msg Text to display
+   * @param options Snackbar options object
+   * @returns SnackbarKey, string|number
+   */
   success(msg: string, options: OptionsObject = {}): SnackbarKey {
     return this.toast(msg, { ...options, variant: "success" });
   },
+  /**
+   * Enqueue a yellow/orange snackbar notification (warning)
+   * @param msg Text to display
+   * @param options Snackbar options object
+   * @returns SnackbarKey, string|number
+   */
   warning(msg: string, options: OptionsObject = {}): SnackbarKey {
     return this.toast(msg, { ...options, variant: "warning" });
   },
+  /**
+   * Enqueue a blue snackbar notification (info)
+   * @param msg Text to display
+   * @param options Snackbar options object
+   * @returns SnackbarKey, string|number
+   */
   info(msg: string, options: OptionsObject = {}): SnackbarKey {
     return this.toast(msg, { ...options, variant: "info" });
   },
+  /**
+   * Enqueue a red snackbar notification (error)
+   * @param msg Text to display
+   * @param options Snackbar options object
+   * @returns SnackbarKey, string|number
+   */
   error(msg: string, options: OptionsObject = {}): SnackbarKey {
     return this.toast(msg, { ...options, variant: "error" });
   },
+  /**
+   * Close a snackbar using the given key
+   * @param key string|number of the notification to dismiss
+   */
   closeSnackbar(key: SnackbarKey): void {
     snackbarRef.closeSnackbar(key);
   },
+  /**
+   * Enqueue a snackbar using the given message and options
+   * @param msg Text to display
+   * @param options Snackbar options
+   * @returns SnackbarKey, string|number
+   */
   toast(msg: string, options: OptionsObject = {}): SnackbarKey {
     return snackbarRef.enqueueSnackbar(msg, options);
   },
--- src/alert/globalalertprovider.tsx
+++ src/alert/globalalertprovider.tsx
@@ -1,9 +1,13 @@
 import React, { CSSProperties } from "react";
 
+// External Alert Utilities
 import { SnackbarKey, SnackbarProvider } from "notistack";
 
+// Internal Alert Utilities
 import snackActions, { SnackbarUtilsConfigurator } from "./alert";
 
+// This style creates the button for dismissing a notification
+// full width, full transparent btn to click anywhere on the notification
 const transparentBtnStyle: CSSProperties = {
   height: "100%",
   left: 0,
@@ -15,14 +19,34 @@ const transparentBtnStyle: CSSProperties = {
   outline: "none",
 };
 
+/**
+ * A top level Component that Provides a snackbar context and
+ * snackbar area, and the framework to allow notifications
+ * throughout the app
+ */
 export default class GlobalAlertProvider extends SnackbarProvider {
+  /**
+   * Wrapper function so it can be used as onClick
+   *
+   * @param key the snackbarkey of the notification to dismiss
+   * @returns an onClick-compatible dismiss function
+   */
   dismissNotification = (key: SnackbarKey): (() => void) => {
     return () => {
       snackActions.closeSnackbar(key);
     };
   };
 
-  dismissAction = (key: SnackbarKey): React.ReactElement => {
+  /**
+   * Provides the element to render for dismissal. In this case,
+   * we use the transparent CSS to create the effect of dismissal
+   * by clicking anywhere on the notification (instead of a
+   * clickable "Dismiss" text)
+   *
+   * @param key the SnackbarKey of the notification to dismiss
+   * @returns React.ReactElement to render
+   */
+  dismissElement = (key: SnackbarKey): React.ReactElement => {
     return (
       <button
         type="button"
@@ -32,10 +56,14 @@ export default class GlobalAlertProvider extends SnackbarProvider {
     );
   };
 
+  /**
+   * @returns Children element, wrapped in the global alert context
+   */
   render(): React.ReactElement {
+    // Take the children out of the props, so children are rendered
     const { children, ...other } = this.props;
     return (
-      <SnackbarProvider {...other} action={this.dismissAction}>
+      <SnackbarProvider {...other} action={this.dismissElement}>
         <SnackbarUtilsConfigurator />
         {children}
       </SnackbarProvider>
