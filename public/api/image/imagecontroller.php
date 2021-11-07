39bc9f3288a2a2fc20ff1ef8ae5e3f615839c250
John McCarthy <jemccarthy13@gmail.com>
2021-11-07 15:33:47 +0100

    formatting php; added protection to image get

--- public/api/image/imagecontroller.php
+++ public/api/image/imagecontroller.php
@@ -5,99 +5,102 @@ require "../login/token.php";
 
 class ImageController
 {
-    private $con;
-    private $requestMethod;
-    private $id;
+  private $con;
+  private $requestMethod;
+  private $id;
 
-    private $imageGateway;
+  private $imageGateway;
 
-    public function __construct($db, $requestMethod)
-    {
-        $this->con = $db;
-        $this->requestMethod = $requestMethod;
+  public function __construct($db, $requestMethod)
+  {
+    $this->con = $db;
+    $this->requestMethod = $requestMethod;
 
-        $this->imageGateway = new ImageGateway($db);
-    }
+    $this->imageGateway = new ImageGateway($db);
+  }
 
-    public function processRequest()
-    {
-        switch ($this->requestMethod) {
-            case "POST":
-                $response = $this->getImagesFromRequest();
-                break;
-            case "PUT":
-                validateToken();
-                $response = $this->putImageFromRequest();
-                break;
-            case "DELETE":
-                validateToken();
-                $response = $this->deleteImageFromRequest();
-                break;
-            case "OPTIONS":
-                $response["status_code_header"] = "HTTP/1.1 200 OK";
-                break;
-            default:
-                $response = notFoundResponse();
-                break;
-        }
-
-        header($response["status_code_header"]);
-        if ($response["body"]) {
-            echo $response["body"];
-        }
-    }
-
-    private function getImagesFromRequest()
-    {
-        $input = json_decode(file_get_contents("php://input"), true);
-        $dir_to_read = $input["directory"];
-
-        if ($dir_to_read == "") {
-            return notFoundResponse();
-        }
-
-        $arr = $this->imageGateway->get($dir_to_read);
-
+  public function processRequest()
+  {
+    switch ($this->requestMethod) {
+      case "POST":
+        $response = $this->getImagesFromRequest();
+        break;
+      case "PUT":
+        validateToken();
+        $response = $this->putImageFromRequest();
+        break;
+      case "DELETE":
+        validateToken();
+        $response = $this->deleteImageFromRequest();
+        break;
+      case "OPTIONS":
         $response["status_code_header"] = "HTTP/1.1 200 OK";
-        $response["body"] = json_encode($arr);
-        return $response;
+        break;
+      default:
+        $response = notFoundResponse();
+        break;
     }
 
-    private function putImageFromRequest()
-    {
-        $input = json_decode(file_get_contents("php://input"), true);
-        $destination = $input["directory"];
-        $file = $input["file"];
-        $filename = $input["filename"];
+    header($response["status_code_header"]);
+    if ($response["body"]) {
+      echo $response["body"];
+    }
+  }
 
-        if($destination===NULL || $file===NULL || $filename===NULL){
-            return unprocessableEntityResponse();
-        }
+  private function getImagesFromRequest()
+  {
+    $input = json_decode(file_get_contents("php://input"), true);
+    $dir_to_read = $input["directory"];
 
-        $result = $this->imageGateway->put($destination, $filename, $file);
-        
-        if (!$result){
-            return internalServerError();
-        }
-        
-        $response["status_code_header"] = "HTTP/1.1 200 OK";
-        return $response;
+    if ($dir_to_read == "") {
+      return notFoundResponse();
+    }
+    if (!is_dir($_SERVER["DOCUMENT_ROOT"] . "/images/" . $dir_to_read)) {
+      return unprocessableEntityResponse();
     }
 
-    private function deleteImageFromRequest()
-    {
-        $input = json_decode(file_get_contents("php://input"), true);
-        $file = $input["file"];
+    $arr = $this->imageGateway->get($dir_to_read);
 
-        if ($file == null) {
-            return unprocessableEntityResponse();
-        }
-        $result = $this->imageGateway->delete($file);
-        if (!$result) {
-            return unprocessableEntityResponse();
-        }
-        $response["status_code_header"] = "HTTP/1.1 200 OK";
-        return $response;
+    $response["status_code_header"] = "HTTP/1.1 200 OK";
+    $response["body"] = json_encode($arr);
+    return $response;
+  }
+
+  private function putImageFromRequest()
+  {
+    $input = json_decode(file_get_contents("php://input"), true);
+    $destination = $input["directory"];
+    $file = $input["file"];
+    $filename = $input["filename"];
+
+    if ($destination === null || $file === null || $filename === null) {
+      return unprocessableEntityResponse();
     }
+
+    $result = $this->imageGateway->put($destination, $filename, $file);
+
+    if (!$result) {
+      return internalServerError();
+    }
+
+    $response["status_code_header"] = "HTTP/1.1 200 OK";
+    return $response;
+  }
+
+  private function deleteImageFromRequest()
+  {
+    $input = json_decode(file_get_contents("php://input"), true);
+    $file = $input["file"];
+
+    if ($file == null) {
+      return unprocessableEntityResponse();
+    }
+    $result = $this->imageGateway->delete($file);
+    if (!$result) {
+      return unprocessableEntityResponse();
+    }
+    $response["status_code_header"] = "HTTP/1.1 200 OK";
+    return $response;
+  }
 }
 ?>
--- public/api/image/imagegateway.php
+++ public/api/image/imagegateway.php
@@ -1,42 +1,67 @@
 <?php
 
-class ImageGateway {
-    private $con=null;
+class ImageGateway
+{
+  private $con = null;
 
-    public function __construct($con){
-        $this->con=$con;
-    }
+  public function __construct($con)
+  {
+    $this->con = $con;
+  }
 
-    public function get($dir_to_read){
-        $dir=$_SERVER['DOCUMENT_ROOT'].'/images/'.$dir_to_read;
+  /**
+   * Process a "get" request.
+   * Precondition:
+   * - $dir_to_read is a valid directory (this function makes no check)
+   * - $dir_to_read contains 0+ images
+   * @return array of images in $dir_to_read on the server
+   */
+  public function get($dir_to_read)
+  {
+    $dir = $_SERVER["DOCUMENT_ROOT"] . "/images/" . $dir_to_read;
 
-        $fileSystemIterator = new FilesystemIterator($dir);
-        $entries = array();
-        foreach ($fileSystemIterator as $fileInfo){
-            if(!file_exists($fileInfo->getPathname())) echo "File not found.";
-            $imgSize=getimagesize($fileInfo->getPathname());
-            $myObj = new \stdClass();
-            $myObj->width=$imgSize[0];
-            $myObj->height=$imgSize[1];
-            $myObj->src="/images/".$dir_to_read."/".$fileInfo->getFilename();
-            $myObj->srcSet="";
-            $myObj->title=$fileInfo->getFilename();
-            $entries[] = $myObj;
+    $fileSystemIterator = new FilesystemIterator($dir);
+    $entries = [];
+    foreach ($fileSystemIterator as $fileInfo) {
+      // continue if file exists
+      if (file_exists($fileInfo->getPathname())) {
+        // continue if mime is image/*
+        // get array should only include images
+        $cntTypeStr = mime_content_type($fileInfo->getPathname());
+        if (preg_match("/image\/*/i", $cntTypeStr)) {
+          $imgSize = getimagesize($fileInfo->getPathname());
+          $myObj = new \stdClass();
+          $myObj->width = $imgSize[0];
+          $myObj->height = $imgSize[1];
+          $myObj->src =
+            "/images/" . $dir_to_read . "/" . $fileInfo->getFilename();
+          $myObj->srcSet = "";
+          $myObj->title = $fileInfo->getFilename();
+          $entries[] = $myObj;
         }
-        return $entries;
+      }
     }
+    return $entries;
+  }
 
-    public function delete($file){
-        $file_url=$_SERVER['DOCUMENT_ROOT'].$file;
-        return unlink($file_url);
-    }
+  public function delete($file)
+  {
+    $file_url = $_SERVER["DOCUMENT_ROOT"] . $file;
+    return unlink($file_url);
+  }
 
-    public function put($destination, $filename, $file){
-        $result = file_put_contents(
-            $_SERVER['DOCUMENT_ROOT']."/images"."/".$destination."/".$filename,
-            file_get_contents($file)
-        );
-        return !($result===false);
-    }
+  public function put($destination, $filename, $file)
+  {
+    $result = file_put_contents(
+      $_SERVER["DOCUMENT_ROOT"] .
+        "/images" .
+        "/" .
+        $destination .
+        "/" .
+        $filename,
+      file_get_contents($file)
+    );
+    return !($result === false);
+  }
 }
 ?>
