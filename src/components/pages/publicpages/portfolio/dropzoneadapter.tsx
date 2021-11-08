ebc11859aaa058a61b7773a4a02b5a5f3c370726
John McCarthy <jemccarthy13@gmail.com>
2021-11-08 06:57:16 +0100

    commenting

--- src/components/pages/publicpages/portfolio/dropzoneadapter.tsx
+++ src/components/pages/publicpages/portfolio/dropzoneadapter.tsx
@@ -1,34 +1,62 @@
-import React from "react";
+// External Utilities
 import { Cookies } from "react-cookie-consent";
 import Dropzone, {
   IFileWithMeta,
   IUploadParams,
 } from "react-dropzone-uploader";
 import "react-dropzone-uploader/dist/styles.css";
+
+// Internal Utilities
 import SnackActions from "../../../../alert/alert";
 
+/**
+ * Expose to the rest of the app the properties required to make
+ * the dropzone-uploader
+ * @member {string} destination directory to place the files
+ */
 interface DAProps {
   destination: string;
 }
 
+/**
+ * Wrap react-dropzone-uploader so we can plug and play third party
+ * libraries if required.
+ *
+ * @param props The external properties required to make the dropzone work
+ * @returns UI Dropzone Component
+ */
 export default function DropzoneAdapter(props: DAProps) {
-  function readFileAsync(file: File) {
-    return new Promise((resolve, reject) => {
-      let reader = new FileReader();
+  /**
+   * Utility function to get file contents
+   * @param file File to read
+   * @returns An awaitable Promise, that returns the contents of the file
+   */
+  function readFileAsync(file: File): Promise<string | ArrayBuffer | null> {
+    return new Promise(
+      (resolve: (v: string | ArrayBuffer | null) => any, reject) => {
+        let reader = new FileReader();
 
-      reader.onload = () => {
-        resolve(reader.result);
-      };
+        reader.onload = () => {
+          resolve(reader.result);
+        };
 
-      reader.onerror = reject;
+        reader.onerror = reject;
 
-      reader.readAsDataURL(file);
-    });
+        reader.readAsDataURL(file);
+      }
+    );
   }
 
+  /**
+   * Override the upload parameters with our own, to allow the backend
+   * api to correctly process the request.
+   *
+   * @param f a File with some metadata added by the Dropzone library
+   * @returns {IUploadParams} mainly, URL/Method, and file contents in body
+   */
   const getUploadParams = async (f: IFileWithMeta): Promise<IUploadParams> => {
+    // turn the file into a string
     const content = await readFileAsync(f.file);
-
     const bstring = JSON.stringify({
       file: content,
       directory: props.destination,
@@ -44,14 +72,20 @@ export default function DropzoneAdapter(props: DAProps) {
     };
   };
 
-  // called every time a file's `status` changes
-  const handleChangeStatus = (m: any, status: any) => {
+  /**
+   * Function called whenever a file's status is changed. We monitor
+   * the status change for error status / completion.
+   *
+   * @param metafile The file with metadata added by the Dropzone.
+   * @param status String status of the file's upload request
+   */
+  const handleChangeStatus = (metafile: IFileWithMeta, status: string) => {
     if (status === "error_upload") {
-      SnackActions.error("Error uploading: " + m.file.name);
+      SnackActions.error("Error uploading: " + metafile.file.name);
     } else if (status === "rejected_file_type") {
       SnackActions.warning("Only images can be submitted.");
     } else if (status === "done") {
-      SnackActions.success("Uploaded " + m.file.name);
+      SnackActions.success("Uploaded " + metafile.file.name);
     }
   };
 
